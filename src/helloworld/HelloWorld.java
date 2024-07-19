7c550516e79665027bc75b21e14ba2bd4c2eb060
ckfghu453 <ckonef1@163.com>
2024-07-19 11:26:32 +0800

    test

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,6 +3,6 @@ package helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hi, Hello World 略做一下变更");
 	}
 }
